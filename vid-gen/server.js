86779a5b8f6c14a0676cb66e658e45f90e54d8d1
Minjoo Song <mnjoo.song@gmail.com>
2025-05-30 09:50:14 -0500

    shorts

--- vid-gen/server.js
+++ vid-gen/server.js
@@ -4,6 +4,7 @@ const fs       = require('fs');
 const path     = require('path');
 const http     = require('http');
 const socketIO = require('socket.io');
+const { channel } = require('diagnostics_channel');
 
 /* ─────────── setup ─────────── */
 const app    = express();
@@ -14,6 +15,11 @@ const mediaBase = path.join(__dirname, 'public', 'media');
 
 app.use(express.static('public'));
 app.use('/media', express.static(mediaBase));
+channel_map = {
+  'channel2': 'PLsBtGCfy7xDYZfARrzPIyF_-YSCymi3RX',
+  'channel4': 'PLsBtGCfy7xDb7qLZXnduwuhP_ms3k5H2X',
+  'channel7': 'PLsBtGCfy7xDbRTDVha223g6fkGLM-v21S',
+};
 
 /* ─────────── sockets ─────────── */
 io.on('connection', (socket) => {
@@ -24,14 +30,15 @@ io.on('connection', (socket) => {
     console.log('[socket] channel:', channelName);
 
     /* hard-wired playlist for channel2 */
-    if (channelName === 'channel2') {
-      io.emit('show-playlist', 'PLsBtGCfy7xDYZfARrzPIyF_-YSCymi3RX');
+    if (channelName in channel_map) {
+      io.emit('show-playlist', channel_map[channelName]);
       return;
     }
 
-    /* otherwise send N random images for slideshow */
-    const folder = path.join(mediaBase, channelName);
-    fs.readdir(folder, (err, files) => {
+    else {
+      /* otherwise send N random images for slideshow */
+      const folder = path.join(mediaBase, channelName);
+      fs.readdir(folder, (err, files) => {
       if (err) { console.error(err); return; }
 
       const imgs = files.filter(f => /\.(jpg|jpeg|png|gif|mp4|webm|ogg)$/i.test(f));
@@ -39,12 +46,14 @@ io.on('connection', (socket) => {
 
       const N = 6;                                         // how many to send
       const sample = imgs
-        .sort(() => 0.5 - Math.random())                   // shuffle
-        .slice(0, N)                                       // take N
-        .map(f => `/media/${channelName}/${f}`);           // build URLs
+      .sort(() => 0.5 - Math.random())                   // shuffle
+      .slice(0, N)                                       // take N
+      .map(f => `/media/${channelName}/${f}`);           // build URLs
 
       io.emit('media-files', sample);                      // broadcast array
-    });
+      });
+    }
+    
   });
 
   /* search bar query ------------------------------------------------------- */
